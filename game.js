65fc68a3d7ec949ee456e344b2be85c36d44efeb
sswarr <samuel_swarr@brown.edu>
2012-04-03 00:46:55 -0400

    Word finder is fairly functional now. Needs some sleek styling. Would also be cool to add directional arrows on how to form words.

--- game.js
+++ game.js
@@ -12,6 +12,7 @@ function Game(){
 	// instance variables
 	this.ctx;
 	this.letters;
+	this.multipliers;
 	this.trie;
 	this.words;
 	
@@ -38,7 +39,12 @@ function Game(){
 		// Enter key
 		if (e.which === 13) {
 			//make sure all letters are filled in
-			console.log(this.game.allLettersValid());
+			if(this.game.allLettersValid()) {
+				this.game.words = {};
+				this.game.words_array = [];
+				this.game.findWords();
+				this.game.wordsToHTML();
+			}
 		}
 	}
 }
@@ -46,15 +52,21 @@ function Game(){
 // initialize the Game fields
 Game.prototype.init = function() {
 	var canvas = $('#mainCanvas')[0];
+	canvas.addEventListener("mousedown", react, false);
     if(canvas.getContext){
     	/* This is the 2d rendering context you will be drawing on. */
 		this.ctx = canvas.getContext('2d');
 		// [<letter>, <letter multiplier>, <word multiplier>]
-		this.letters = [[['r',1,1],['r',1,1],['e',1,1],['d',2,1]],
-						[['u',1,1],['s',1,1],['a',1,1],['p',1,1]],
-						[['b',1,1],['e',1,2],['i',1,1],['d',1,1]],
-						[['y',1,1],['t',2,1],['v',1,1],['u',1,1]]];
+		this.letters = [[['f',1,1],['u',1,1],['n',1,1],['o',1,1]],
+						[['r',1,1],['w',1,1],['d',1,1],['t',3,1]],
+						[['o',1,1],['a',3,1],['e',3,1],['m',1,1]],
+						[['b',1,1],['l',1,1],['t',1,3],['n',1,1]]];
 		
+		// 0 = none, 1 = dl, 2 = tl, 3 = dw, 4 = tw
+		this.multipliers = [[0,0,0,0],
+							[0,0,0,2],
+							[0,2,2,0],
+							[0,0,4,0]];
 		
 		CANVAS_WIDTH = this.ctx.canvas.width;
 		CANVAS_HEIGHT = this.ctx.canvas.height;
@@ -69,8 +81,7 @@ Game.prototype.init = function() {
 		console.log("TRIE CONSTRUCTED");
 		
 		this.words = {};
-		
-		this.findWords();
+		this.words_array = [];
 		
 		this.r = 0;
 		this.c = 0;
@@ -79,6 +90,7 @@ Game.prototype.init = function() {
 	}
 	return false;
 }
+
 
 Game.prototype.allLettersValid = function() {
 	for (var i = 0; i < 4; i++) {
@@ -96,6 +108,36 @@ Game.prototype.findWords = function() {
 		for (var j = 0; j < num_cols; j++) {
 			this.find_permutations(i, j, this.letters, num_cols, num_rows);
 		}
+	}
+	this.words_array.sort(compareWordsByPoints);
+}
+
+// function to compare words in the form [<word>, <point value>, <coord sequence>]
+// compare point values first, if they're equal sort by word alphabetically
+function compareWordsByPoints(a, b) {
+	if (a[1] > b[1]) { return -1;}
+	else if (a[1] < b[1]) { return 1;}
+	else {
+		if (a[0] > b[0]) { return 1;}
+		else if (a[0] < b[0]) { return -1;}
+		else { return 0;}
+	}
+}
+
+Game.prototype.wordsToHTML = function() {
+	$( "#results" ).html(			
+		"<tr>" +
+			"<td>Word</td>" +
+			"<td>Length</td>" +
+			"<td>Points</td>" +
+		"</tr>");
+	for (var i = 0; i < this.words_array.length; i++) {
+		$( "#results" ).append(			
+			"<tr>" +
+				"<td>" + this.words_array[i][0] + "</td>" +
+				"<td>" + this.words_array[i][0].length + "</td>" + 
+				"<td>" + this.words_array[i][1] + "</td>" +
+			"</tr>");
 	}
 }
 
@@ -115,10 +157,13 @@ Game.prototype.find_permutations = function(r, c, chars, width, height) {
 	// start with the first letter
 	var cumul_string = chars[r][c][0];
 	
-	this.find_permutations_helper(r, c, chars, used, width, height, cumul_string, letter_values[chars[r][c][0]] * chars[r][c][1], chars[r][c][2]);
+	// start with the first letter's location
+	var cumul_coords = [[r,c]];
+	
+	this.find_permutations_helper(r, c, chars, used, width, height, cumul_string, cumul_coords, letter_values[chars[r][c][0]] * chars[r][c][1], chars[r][c][2]);
 }
 
-Game.prototype.find_permutations_helper = function(r, c, chars, used, width, height, cumul_string, point_value, multiplier) {
+Game.prototype.find_permutations_helper = function(r, c, chars, used, width, height, cumul_string, cumul_coords, point_value, multiplier) {
 	// find the coords all the neighboring cells that haven't been used yet
 	var valid_neighbors = this.find_unused_neighbors(r, c, used, width, height);
 	
@@ -133,15 +178,19 @@ Game.prototype.find_permutations_helper = function(r, c, chars, used, width, hei
 	for (var n = 0; n < valid_neighbors.length; n++) {
 		// create the string that would be formed with this neighbor
 		var string_copy = cumul_string + chars[valid_neighbors[n][0]][valid_neighbors[n][1]][0];
+		var coords_copy = cumul_coords.slice(0);
+		coords_copy.push([valid_neighbors[n][0],valid_neighbors[n][1]]);
 		var new_point_value = point_value + letter_values[chars[valid_neighbors[n][0]][valid_neighbors[n][1]][0]] * chars[valid_neighbors[n][0]][valid_neighbors[n][1]][1];
 		var new_multiplier = multiplier * chars[valid_neighbors[n][0]][valid_neighbors[n][1]][2];
 		// add it to this.words if its a word
 		if (this.trie.containsWord(string_copy)) { 
-			this.words[string_copy] = new_point_value * new_multiplier + length_bonus[string_copy.length]; 
+			var points = (string_copy.length === 2) ? 1 : new_point_value;
+			this.words[string_copy] = [points * new_multiplier + length_bonus[string_copy.length], coords_copy]; 
+			this.words_array.push([string_copy, points * new_multiplier + length_bonus[string_copy.length], coords_copy]);
 		}
 		// continue on this branch of recursion only if the prefix accumulated so far is in the trie
 		if (this.trie.containsPrefix(string_copy)) {
-			this.find_permutations_helper(valid_neighbors[n][0], valid_neighbors[n][1], chars, used_copy, width, height, string_copy, new_point_value, new_multiplier);
+			this.find_permutations_helper(valid_neighbors[n][0], valid_neighbors[n][1], chars, used_copy, width, height, string_copy, coords_copy, new_point_value, new_multiplier);
 		}
 	}
 	
@@ -197,13 +246,38 @@ Game.prototype.draw = function() {
 	for (var i = 0; i < 4; i++) {
 		for (var j = 0; j < 4; j++) {
 			this.ctx.strokeStyle = "rgb(0,0,0)";
-			this.ctx.fillStyle = "rgb(100,100,100)";
-			this.ctx.strokeRect(j*CANVAS_WIDTH/4 + 3*CANVAS_HEIGHT/16, i*CANVAS_HEIGHT/4 + 3*CANVAS_HEIGHT/16, CANVAS_WIDTH/16, CANVAS_HEIGHT/16);
-			this.ctx.fillRect(j*CANVAS_WIDTH/4 + 3*CANVAS_HEIGHT/16, i*CANVAS_HEIGHT/4 + 3*CANVAS_HEIGHT/16, CANVAS_WIDTH/16, CANVAS_HEIGHT/16);
+			this.ctx.fillStyle = "rgb(160,160,160)";
+			this.ctx.strokeRect(j*CANVAS_WIDTH/4 + 3*CANVAS_HEIGHT/16 - 1, i*CANVAS_HEIGHT/4 + 3*CANVAS_HEIGHT/16 - 1, CANVAS_WIDTH/16, CANVAS_HEIGHT/16);
+			this.ctx.fillRect(j*CANVAS_WIDTH/4 + 3*CANVAS_HEIGHT/16 - 1, i*CANVAS_HEIGHT/4 + 3*CANVAS_HEIGHT/16 - 1, CANVAS_WIDTH/16, CANVAS_HEIGHT/16);
+		
+			this.ctx.fillStyle = "rgb(0,0,0)";
+			this.ctx.font = "14px Arial";
+			this.ctx.textAlign = "start";
+			this.ctx.textBaseline = "top";
+			switch (this.multipliers[i][j]) {
+				case 1:
+					this.ctx.fillText('DL', j*CANVAS_WIDTH/4 + 3*CANVAS_HEIGHT/16, i*CANVAS_HEIGHT/4 + 3*CANVAS_HEIGHT/16 + 3);
+					break;
+				case 2:
+					this.ctx.fillText('TL', j*CANVAS_WIDTH/4 + 3*CANVAS_HEIGHT/16 , i*CANVAS_HEIGHT/4 + 3*CANVAS_HEIGHT/16 + 3);
+					break;
+				case 3:
+					this.ctx.font = "12px Arial";
+					this.ctx.fillText('DW', j*CANVAS_WIDTH/4 + 3*CANVAS_HEIGHT/16 - 1, i*CANVAS_HEIGHT/4 + 3*CANVAS_HEIGHT/16 + 4);
+					break;
+				case 4:
+					this.ctx.font = "12px Arial";
+					this.ctx.fillText('TW', j*CANVAS_WIDTH/4 + 3*CANVAS_HEIGHT/16 - 1, i*CANVAS_HEIGHT/4 + 3*CANVAS_HEIGHT/16 + 4);
+					break;
+				default:
+					break;
+			}
+			
 		}
 	}
 	for (var i = 0; i < 4; i++) {
 		for (var j = 0; j < 4; j++) {
+			this.ctx.fillStyle = "rgb(0,0,0)";
 			this.ctx.font = "40px Arial";
 			this.ctx.textAlign = "center";
 			this.ctx.textBaseline = "middle";
@@ -215,4 +289,69 @@ Game.prototype.draw = function() {
 // main game logic method, called at 60fps
 Game.prototype.update = function() {
 	
+}
+
+function react(event) {
+	
+	var coords = getPosition(event);
+	var rc = [Math.floor(coords[1]/(CANVAS_HEIGHT/4)), Math.floor(coords[0]/(CANVAS_WIDTH/4))];
+	coords[0] = coords[0]%(CANVAS_WIDTH/4);
+	coords[1] = coords[1]%(CANVAS_HEIGHT/4);
+	if (coords[0] >= 60 && coords[0] <= 80 && coords[1] >= 60 && coords[1] <= 80) {
+		game.multipliers[rc[0]][rc[1]] = (game.multipliers[rc[0]][rc[1]] + 1) % 5;
+		switch (game.multipliers[rc[0]][rc[1]]) {
+			case 0:
+				game.letters[rc[0]][rc[1]][1] = 1;
+				game.letters[rc[0]][rc[1]][2] = 1;
+				break;
+			case 1:
+				game.letters[rc[0]][rc[1]][1] = 2;
+				game.letters[rc[0]][rc[1]][2] = 1;
+				break;
+			case 2:
+				game.letters[rc[0]][rc[1]][1] = 3;
+				game.letters[rc[0]][rc[1]][2] = 1;
+				break;
+			case 3:
+				game.letters[rc[0]][rc[1]][1] = 1;
+				game.letters[rc[0]][rc[1]][2] = 2;
+				break;
+			case 4:
+				game.letters[rc[0]][rc[1]][1] = 1;
+				game.letters[rc[0]][rc[1]][2] = 3;
+				break;
+			default:
+				game.letters[rc[0]][rc[1]][1] = 1;
+				game.letters[rc[0]][rc[1]][2] = 1;
+				break;
+		}
+	} else {
+		game.r = rc[0];
+		game.c = rc[1];
+	}
+	game.draw();
+}
+
+ function getPosition(event) {
+        var x = new Number();
+        var y = new Number();
+        var canvas = $('#mainCanvas')[0];
+
+        if (event.x != undefined && event.y != undefined)
+        {
+          x = event.x;
+          y = event.y;
+        }
+        else // Firefox method to get the position
+        {
+          x = event.clientX + document.body.scrollLeft +
+              document.documentElement.scrollLeft;
+          y = event.clientY + document.body.scrollTop +
+              document.documentElement.scrollTop;
+        }
+
+        x -= canvas.offsetLeft;
+        y -= canvas.offsetTop;
+
+        return [x, y];
 }
