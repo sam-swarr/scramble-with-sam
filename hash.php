c933561566e6be35025067bc5b7b202b2333749d
sswarr <samuel_swarr@brown.edu>
2012-04-15 19:55:03 -0400

    solve.php is now capable of outputting array with results. need to now have it read from GET. also need to remove duplicates either in backend or front end. need to add ajax

--- hash.php
+++ hash.php
@@ -1,10 +1,67 @@
 <?php
+
+$hash2l = unserialize(file_get_contents("cached_hash2l"));
+
 $hash = unserialize(file_get_contents("cached_hash"));
 
-function contains(&$hash, $word) {
-	foreach ($hash[substr($word, 0, 3)] as $curr) {
-		if ($word === $curr) { return true; }
+$prefix6 = unserialize(file_get_contents("cached_prefix6"));
+
+$prefix9 = unserialize(file_get_contents("cached_prefix9"));
+
+$prefix12 = unserialize(file_get_contents("cached_prefix12"));
+
+$prefix14 = unserialize(file_get_contents("cached_prefix14"));
+
+
+
+function contains($word) {
+	global $hash;
+	if ($hash[substr($word, 0, 3)]) {
+		foreach ($hash[substr($word, 0, 3)] as $curr) {
+			if ($word === $curr) { return true; }
+		}
 	}
 	return false;
 }
+
+/*
+$hash2l = array();
+for ($i = 0; $i < count($dict); $i++) {
+	if (strlen($dict[$i]) === 2) {
+		$hash2l[$dict[$i]] = true;
+	}
+}
+
+file_put_contents("cached_hash2l", serialize($hash2l));
+*/
+$letter_values = array(
+"a" => 1,
+"b" => 4,
+"c" => 4,
+"d" => 2,
+"e" => 1,
+"f" => 4,
+"g" => 3,
+"h" => 3,
+"i" => 1,
+"j" => 10,
+"k" => 5,
+"l" => 2,
+"m" => 4,
+"n" => 2,
+"o" => 1,
+"p" => 4,
+"q" => 10,
+"r" => 1,
+"s" => 1,
+"t" => 1,
+"u" => 2,
+"v" => 5,
+"w" => 4,
+"x" => 8,
+"y" => 3,
+"z" => 10
+);
+
+$length_bonus = array(0,0,0,0,0,3,6,10,15,20,30,50,80,150,500,1000,30000);
 ?>
--- solve.php
+++ solve.php
@@ -7,102 +7,94 @@ $letters = array(array('f',1,1), array('u',1,1), array('n',1,1), array('o',1,1),
 				 array('r',1,1), array('w',1,1), array('d',1,1), array('t',3,1),
 				 array('o',1,1), array('a',3,1), array('e',3,1), array('m',1,1),
 				 array('b',1,1), array('l',1,1), array('t',1,3), array('n',1,1));
+				 
+/*$letters = array(array('a',1,1), array('b',1,1), array('r',1,1), array('a',1,1),
+				 array('e',1,1), array('v',1,1), array('i',1,1), array('s',3,1),
+				 array('n',1,1), array('e',3,1), array('s',3,1), array('s',1,1),
+				 array('n',1,1), array('o',1,1), array('s',1,3), array('e',1,1));*/
 
 function ind($row, $col) {
 	return 4*$row + $col;
 }
 
-// finds all words that can be formed starting at location (r, c) on the 
-// array of letters, chars, with the given width and height and adds them to this.words
-Game.prototype.find_permutations = function(r, c, chars, width, height) {
-	// create boolean array of the same size as chars to represent which letters have been used
-	var used = new Array(height);
-	for (var i = 0; i < height; i++) {
-		used[i] = new Array(width);
-		for (var j = 0; j < width; j++) {
-			used[i][j] = false;
-		}
-	}
-	used[r][c] = true;
-	
-	// start with the first letter
-	var cumul_string = chars[r][c][0];
-	
-	// start with the first letter's location
-	var cumul_coords = [[r,c]];
-	
-	this.find_permutations_helper(r, c, chars, used, width, height, cumul_string, cumul_coords, letter_values[chars[r][c][0]] * chars[r][c][1], chars[r][c][2]);
-}
+$words_found = array();
 
+// finds all words that can be formed starting at location (r, c) on the 
+// array of letters, chars, and adds them to $words_found
 function find_perms($r, $c, $chars) {
+	global $letter_values;
+
+	// create boolean array to represent which letters have been used
 	$used = array();
 	$used[ind($r,$c)] = true;
 	
+	// start with the first letter
 	$cumul_string = $chars[ind($r,$c)][0];
 	
-	$cumul_coords = array(array($r, $c));
+	// start with the first letter's location
+	//$cumul_coords = array(array($r, $c));
 	
-	find_perms_helper($r, $c, $chars, $used, $cumul_string, $cumul_coords, 
+	find_perms_helper($r, $c, $chars, $used, $cumul_string, $letter_values[$chars[ind($r,$c)][0]] * $chars[ind($r,$c)][1], $chars[ind($r,$c)][2]);
 }
 
-
-Game.prototype.find_permutations_helper = function(r, c, chars, used, width, height, cumul_string, cumul_coords, point_value, multiplier) {
+function find_perms_helper($r, $c, $chars, $used, $cumul_string, $point_value, $multiplier) {
+	global $letter_values, $words_found, $hash, $hash2l, $length_bonus, $prefix6, $prefix9, $prefix12, $prefix14;
+	
+	
 	// find the coords all the neighboring cells that haven't been used yet
-	var valid_neighbors = this.find_unused_neighbors(r, c, used, width, height);
-	
-	// make a copy of the boolean array since multiple branches of recursion will occur
-	var used_copy = new Array(height);
-	for (var i = 0; i < height; i++) {
-		used_copy[i] = used[i].slice(0);
-	}
-	used_copy[r][c] = true;
-	
-	// for each unused neighbor
-	for (var n = 0; n < valid_neighbors.length; n++) {
-		// create the string that would be formed with this neighbor
-		var string_copy = cumul_string + chars[valid_neighbors[n][0]][valid_neighbors[n][1]][0];
-		var coords_copy = cumul_coords.slice(0);
-		coords_copy.push([valid_neighbors[n][0],valid_neighbors[n][1]]);
-		var new_point_value = point_value + letter_values[chars[valid_neighbors[n][0]][valid_neighbors[n][1]][0]] * chars[valid_neighbors[n][0]][valid_neighbors[n][1]][1];
-		var new_multiplier = multiplier * chars[valid_neighbors[n][0]][valid_neighbors[n][1]][2];
-		// add it to this.words if its a word
-		if (this.trie.containsWord(string_copy)) { 
-			var points = (string_copy.length === 2) ? 1 : new_point_value;
-			this.words[string_copy] = [points * new_multiplier + length_bonus[string_copy.length], coords_copy]; 
-			this.words_array.push([string_copy, points * new_multiplier + length_bonus[string_copy.length], coords_copy]);
-		}
-		// continue on this branch of recursion only if the prefix accumulated so far is in the trie
-		if (this.trie.containsPrefix(string_copy)) {
-			this.find_permutations_helper(valid_neighbors[n][0], valid_neighbors[n][1], chars, used_copy, width, height, string_copy, coords_copy, new_point_value, new_multiplier);
-		}
-	}
-	
-}
-
-function find_perms_helper($r, $c, $chars, $used, $cumul_string, $cumul_coords, $point_value, $multiplier) {
 	$valid_neighbors = find_unused_neighbors($r, $c, $used);
 	
+	// make a copy of the boolean array since multiple branches of recursion will occur
+	$used_copy = $used;
+	$used_copy[ind($r,$c)] = true;
 	
-}
-
-
-Game.prototype.find_unused_neighbors = function(r, c, used, width, height) {
-	var result = [];
-	for (var y = -1; y <= 1; y++) {
-		for (var x = -1; x <= 1; x++) {
-			if (y === 0 && x === 0) {continue;}
-			var curr_r = r + y;
-			var curr_c = c + x;
-			if (curr_r >= 0 && curr_r < height) {
-				if(curr_c >= 0 && curr_c < width) {
-					if (!used[curr_r][curr_c]) {
-						result.push([curr_r, curr_c]);
-					}
-				}
-			}
+	// for each unused neighbor
+	foreach ($valid_neighbors as $neighbor) {
+		// create the string that would be formed with this neighbor
+		$string_copy = $cumul_string . $chars[ind($neighbor[0],$neighbor[1])][0];
+		
+		
+		// copy the coords and add the coord of this neighbor
+		//$coords_copy = $cumul_coords;
+		//$coords_copy[] = array($neighbor[0],$neighbor[1]);
+		
+		// calculate the new point value and multiplier
+		$new_point_value = $point_value + $letter_values[$chars[ind($neighbor[0],$neighbor[1])][0]] *$chars[ind($neighbor[0],$neighbor[1])][1];
+		$new_multiplier = $multiplier * $chars[ind($neighbor[0],$neighbor[1])][2];
+		
+		$len = strlen($string_copy);
+		
+		// if the word is two letters long, check the 2 letter hash and add if its a word
+		if ($len === 2 && $hash2l[$string_copy]) {
+			$words_found[] = array($string_copy, $new_multiplier);
 		}
+		// otherwise if the word is three or more letters, check the big hash and add if necessary
+		if ($len > 2 && contains($string_copy)) {
+			$words_found[] = array($string_copy, $new_point_value * $new_multiplier + $length_bonus[$len]);
+		}
+		
+		// continue on this branch of recursion only if the 3-letter prefix accumulated so far is valid
+		
+		if ($len < 3) { // || $hash[substr($string_copy, 0, 3)]) {
+			find_perms_helper($neighbor[0], $neighbor[1], $chars, $used_copy, $string_copy, $new_point_value, $new_multiplier);
+		}
+		else if ($len >= 3 && $len < 6 && $hash[substr($string_copy, 0, 3)]) {
+			find_perms_helper($neighbor[0], $neighbor[1], $chars, $used_copy, $string_copy, $new_point_value, $new_multiplier);
+		}
+		else if ($len >= 6 && $len < 9 && $prefix6[substr($string_copy, 0, 6)]) {
+			find_perms_helper($neighbor[0], $neighbor[1], $chars, $used_copy, $string_copy, $new_point_value, $new_multiplier);
+		}
+		else if ($len >= 9 && $len < 12  && $prefix9[substr($string_copy, 0, 9)]) {
+			find_perms_helper($neighbor[0], $neighbor[1], $chars, $used_copy, $string_copy, $new_point_value, $new_multiplier);
+		}
+		else if ($len >= 12 && $len < 14 && $prefix12[substr($string_copy, 0, 12)]) {
+			find_perms_helper($neighbor[0], $neighbor[1], $chars, $used_copy, $string_copy, $new_point_value, $new_multiplier);
+		}
+		else if ($len >= 14 && $prefix14[substr($string_copy, 0, 14)]) {
+			find_perms_helper($neighbor[0], $neighbor[1], $chars, $used_copy, $string_copy, $new_point_value, $new_multiplier);
+		}
+		else {}
 	}
-	return result;
 }
 
 
@@ -115,12 +107,30 @@ function find_unused_neighbors($r, $c, $used) {
 			$curr_c = $c + $x;
 			if ($curr_r >= 0 && $curr_r < 4) {
 				if ($curr_c >= 0 && $curr_c < 4) {
-				
+					if (!$used[ind($curr_r,$curr_c)]) {
+						$result[] = array($curr_r, $curr_c);
+					}
 				}
 			}
 		}
 	}
+	return $result;
 }
+
+for ($r = 0; $r < 4; $r++) {
+	for ($c = 0; $c < 4; $c++) {
+		find_perms($r,$c,$letters);
+	}
+}
+print_r(json_encode($words_found));
+/*
+$used = array();
+$used[ind(1,1)] = true;
+$used2 = $used;
+$used2[ind(1,2)] = true;
+print_r($used2);*/
+
+//print_r(find_unused_neighbors(1,0,$used));
 
 echo memory_get_usage()/1024/1024;
 ?>
